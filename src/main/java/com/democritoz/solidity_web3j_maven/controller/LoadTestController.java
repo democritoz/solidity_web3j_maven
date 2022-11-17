671755e76c65bb8a4094e33a81554f978a17350d
ziofront+gitlab <ziofront@gmail.com>
2022-11-17 10:57:02 +0900

    IntelliJ 저장시 소스 자동 포매팅

--- src/main/java/com/democritoz/solidity_web3j_maven/controller/LoadTestController.java
+++ src/main/java/com/democritoz/solidity_web3j_maven/controller/LoadTestController.java
@@ -6,7 +6,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -42,19 +45,18 @@ public class LoadTestController {
         try {
             // private 키가... 하드코딩 되는데... 어케 숨길까?
 
-
             // ############ Test001 #############
             // application에서 contract를 deploy할때 마다 주소가 생성 되는데...
             // 이 contract deploy는 언제, 어떻게 해야 하나?
             // 기존의 contract는 어떻게 되나?
             // 기존의 contract 데이터와 연결점이 없겠지...?
 
-
             boolean isValid = test001.isValid();
             log.debug("isValid=[{}]", isValid);
 
             String name = null;
-            if(isValid) {
+            if (isValid) {
+                // setName
                 TransactionReceipt transactionReceipt1 = test001.setName(testDomain.getName()).send();
                 log.debug("transactionReceipt1=[{}]", transactionReceipt1);
                 log.debug("transactionReceipt1.toString()=[{}]", transactionReceipt1.toString());
@@ -62,7 +64,7 @@ public class LoadTestController {
                 responseMap.put("transactionReceipt1", transactionReceipt1.toString());
                 responseMap.put("usedGas1", transactionReceipt1.getGasUsed().intValue());
 
-
+                // setAge
                 TransactionReceipt transactionReceipt2 = test001.setAge(BigInteger.valueOf(testDomain.getAge())).send();
                 log.debug("transactionReceipt2=[{}]", transactionReceipt2);
                 log.debug("transactionReceipt2.toString()=[{}]", transactionReceipt2.toString());
@@ -70,7 +72,7 @@ public class LoadTestController {
                 responseMap.put("transactionReceipt2", transactionReceipt2.toString());
                 responseMap.put("usedGas2", transactionReceipt2.getGasUsed().intValue());
 
-
+                // changeAll
                 TransactionReceipt transactionReceipt3 = test001.changeAll(testDomain.getName(), BigInteger.valueOf(testDomain.getAge())).send();
                 log.debug("transactionReceipt3=[{}]", transactionReceipt3);
                 log.debug("transactionReceipt3.toString()=[{}]", transactionReceipt3.toString());
@@ -78,7 +80,6 @@ public class LoadTestController {
                 responseMap.put("transactionReceipt3", transactionReceipt3.toString());
                 responseMap.put("usedGas3", transactionReceipt3.getGasUsed().intValue());
             }
-
 
             web3j.shutdown();
 
@@ -90,7 +91,6 @@ public class LoadTestController {
 //
 //            log.debug("age=[{}]", age);
 
-
 //            responseMap.put("ethAccounts", ethAccounts);
 //            responseMap.put("credentials", credentials);
 //            responseMap.put("test001", test001);
@@ -99,14 +99,11 @@ public class LoadTestController {
             log.error(e.getMessage(), e);
         }
 
-
         return ResponseEntity.ok(responseMap);
     }
 
-
     @GetMapping("/load/test001")
     public ResponseEntity<Map<String, Object>> getValue() {
-
 
         Map<String, Object> responseMap = new HashMap<>();
 
@@ -125,7 +122,7 @@ public class LoadTestController {
 
             String name = null;
             BigInteger age = BigInteger.ZERO;
-            if(isValid) {
+            if (isValid) {
                 name = test001.getName().send();
                 age = test001.getAge().send();
             }
@@ -133,10 +130,8 @@ public class LoadTestController {
             log.debug("name=[{}]", name);
             log.debug("age=[{}]", age);
 
-
             responseMap.put("name", name);
             responseMap.put("age", age);
-
 
             web3j.shutdown();
 
@@ -148,7 +143,6 @@ public class LoadTestController {
 //
 //            log.debug("age=[{}]", age);
 
-
 //            responseMap.put("ethAccounts", ethAccounts);
 //            responseMap.put("credentials", credentials);
 //            responseMap.put("test001", test001);
@@ -157,9 +151,7 @@ public class LoadTestController {
             log.error(e.getMessage(), e);
         }
 
-
         return ResponseEntity.ok(responseMap);
     }
-
 
 }
